4ad431a96ab4785bffc150d0859f7bdc2a76d64a
Roy <loganroyjr4@gmail.com>
2022-09-01 17:02:30 -0500

    add global variables and start to create function to collect user inputs

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// Assignment Code
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numeric = ['0','1','2','3','4','5','6','7','8','9'];
+var specialCharacters = ['!','@','#','$','%','^','&','*','(',')'];
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -13,6 +17,33 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function userInfo() {
+  // var text;
+  var length = parseInt(prompt('Enter your desired lenght of your password.'));
+    if (length < 8 || length > 128) {
+      alert('You must choose lenght between 8-128.');
+      return null;
+    }
+    if (Number.isNaN(length)) {
+      alert('You must choose a number length between 8-128.');
+      return null;
+    }
+  var confirmLower = confirm('Do you want lowercase characters in your password?');
+  // add remaing confirms
+  if (confirmLower === false) {
+    alert('You must choose atleast one special character.');
+    return null;
+  }
+  // creating object to hold user input
+  var userInputs = {
+    length: length,
+    confirmLower: confirmLower,
+  }
+  return userInputs;
+}
+function generatePassword() {
+  var inputs = userInfo();
+}
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
