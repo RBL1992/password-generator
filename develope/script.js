39988e0af86e4a0175b2aa2c93e1e3efc51c9b27
Roy <loganroyjr4@gmail.com>
2022-09-01 15:21:03 -0500

    added psuedo code commented out

--- develope/script.js
+++ develope/script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+            // promp1 lowercase
+            // promp2 uppercase
+            // promp3 numeric
+            // promp4 special characters
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+            // user input number 8 <= 128 characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+            // if user does not select atleast 1 character alert
+            // otherwise...
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+                        // display password
