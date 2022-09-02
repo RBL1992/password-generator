27a9922d0fdab4368e2b5f737c4eb73148b9532a
Roy <loganroyjr4@gmail.com>
2022-09-01 20:06:51 -0500

    added 2 functions to change lowercase to upper... finished if statements within generate password function

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numeric = ['0','1','2','3','4','5','6','7','8','9'];
-var specialCharacters = ['!','@','#','$','%','^','&','*','(',')'];
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 
 var generateBtn = document.querySelector("#generate");
 
@@ -18,20 +16,20 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function userInfo() {
-  var length = parseInt(prompt('Enter your desired lenght of your password.'));
-    if (length < 8 || length > 128) {
-      alert('You must choose lenght between 8-128.');
-      return null;
-    }
-    if (Number.isNaN(length)) {
-      alert('You must choose a number length between 8-128.');
-      return null;
-    }
+  var length = parseInt(prompt('Enter your desired length of your password between 8 - 128 characters.'));
+  if (length < 8 || length > 128) {
+    alert('You must choose length between 8-128.');
+    return null;
+  }
+  if (Number.isNaN(length)) {
+    alert('You must choose a number length between 8-128.');
+    return null;
+  }
   var confirmLower = confirm('Do you want lowercase characters in your password?');
   var confirmUpper = confirm('Do you want uppercase characters in your password?');
   var confirmNumeric = confirm('Do you want numeric characters in your password?');
   var confirmSpecial = confirm('Do you want special characters in your password?');
- 
+
   if (confirmLower === false && confirmUpper === false && confirmNumeric === false && confirmSpecial === false) {
     alert('You must choose atleast one unique character.');
     return null;
@@ -40,30 +38,55 @@ function userInfo() {
   var userInputs = {
     length: length,
     confirmLower: confirmLower,
+    confirmUpper: confirmUpper,
+    confirmNumeric: confirmNumeric,
+    confirmSpecial: confirmSpecial,
   }
   return userInputs;
 }
-function generatePassword() {
-  var inputs = userInfo();
+
+function convertLowerCaseArrayToUpperCase(arr) {
+  var upperCaseValues = [];
+  arr.forEach(element => {
+    upperCaseValues.push(element.toUpperCase());
+  });
+  return upperCaseValues;
 }
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-            // promp1 lowercase
-            // promp2 uppercase
-            // promp3 numeric
-            // promp4 special characters
+function basenumbers(size) {
+  var result = []
+  for (var i = 0; i < size; i++) {
+    result.push(i);
+  }
+  return result;
+}
+function generatePassword() {
+  var availableCharacters = [];
+  var completedPassword = [];
+  var userInputs = userInfo();
 
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-            // user input number 8 <= 128 characters
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-            // if user does not select atleast 1 character alert
-            // otherwise...
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-                        // display password
+  if (userInputs != null) {
+    if (userInputs.confirmLower === true) {
+      availableCharacters = availableCharacters.concat(lowerCase)
+    }
+    if (userInputs.confirmUpper === true) {
+      var upperCase = convertLowerCaseArrayToUpperCase(lowerCase);
+      availableCharacters = availableCharacters.concat(upperCase);
+    }
+    if (userInputs.confirmNumeric === true) {
+      var numeric = basenumbers(10);
+      availableCharacters = availableCharacters.concat(numeric)
+    }
+    if (userInputs.confirmSpecial === true) {
+      availableCharacters = availableCharacters.concat(specialCharacters)
+    }
+    for (var i = 0; i < userInputs.length; i++) {
+      var randomIndex = Math.floor(Math.random() * availableCharacters.length);
+      completedPassword.push(availableCharacters[randomIndex]);
+    }
+    var result = '';
+    completedPassword.forEach(character => result += character)
+    return result;
+  } else {
+    generatePassword();
+  }
+}
